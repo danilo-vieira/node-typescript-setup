4976ed9d4a80704c69273fde8cfe9c82290a8817
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-06-20 23:00:43 -0300

    config: add TS configuration and paths

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import "@controllers/UsersController";
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (_, response) => {
   return response.json({ ok: true });
